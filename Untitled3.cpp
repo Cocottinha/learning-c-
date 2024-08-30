bb0b3bd06b8070ce8a50e694385daabeae695382
Lucas Cotta Muniz <lucascottamuniz@hotmail.com>
2024-08-30 10:37:09 -0300

    att

--- Untitled3.cpp
+++ Untitled3.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
-        Função iterativa para calcular um termo n da sequência de fibonacci
-        Como a sequência cresce muito rápido, está sendo usado o operador long para que o tipo int ocupe 8 bytes.
-*/
 long long int fiboI(int n){
     long long int a = 0, b = 1, c;
     int i = 2;
@@ -30,7 +26,7 @@ int main () {
     int n = 50;
     time_t tIni, tFim;
 
-    tIni = time(NULL); // obtem a hora do computador
+    tIni = time(NULL);
     printf("Fibonacci iterativo: %lld\n", fiboI(n));
     tFim = time(NULL);
     printf("\tTempo em segundos: %f\n\n", difftime(tFim, tIni)); // diferença entre a hora de início e a hora de fim
